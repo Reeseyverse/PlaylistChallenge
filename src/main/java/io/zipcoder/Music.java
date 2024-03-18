d023333c5c7d316ede93d18c9628a96a0789d5dd
Reeseyverse <jorrisecko@gmail.com>
2024-03-18 12:58:32 -0400

    finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,24 @@
 package io.zipcoder;
+import java. lang.String;
+import java.util.Arrays;
+
+import java.util.ArrayList;
 
 public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        ArrayList<String> pickedSong = new ArrayList<String>(Arrays.asList(this.playList));
+
+        int nextSong = Math.abs(pickedSong.indexOf(selection) - startIndex);
+        int lastSong = Math.abs(pickedSong.size() + startIndex) - pickedSong.lastIndexOf(selection);
+
+        return Math.min(nextSong,lastSong);
     }
 }
+
